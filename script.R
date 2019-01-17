7a9c9f1e596c45a694b9199ad9d2d3148fbd6d15
Dean Hardy <dhardy@Deans-MacBook-Pro.local>
2019-01-16 22:27:52 -0500

    created facet and added legend

--- script.R
+++ script.R
@@ -6,10 +6,13 @@ rm(list=ls())
 
 library(tidyverse)
 library(lubridate)
+library(gridExtra)
+library(cowplot)
 
 #define data directory
 datadir <- file.path('/Users/dhardy/Dropbox/r_data/manuscript-timelines')
 
+## import data
 df <- read.csv(file.path(datadir, "data.csv")) %>%
   mutate_all(as.character) %>%
   mutate(date = as.Date(date, "%m/%d/%y")) %>%
@@ -30,7 +33,7 @@ df <- read.csv(file.path(datadir, "data.csv")) %>%
   arrange(start_date)
   
 
-# cols <- c('red', 'blue')
+## manuscripts
 ms <- df %>%
   filter(status != "published", type == "ms")
 
@@ -40,7 +43,15 @@ fig_ms <- ggplot(ms) +
                      ymin = start_date,
                      color = status),
                  size = 1,
-            filter(ms, status != 'accepted')) +
+            filter(ms, status != 'accepted'),
+            show.legend = F) +
+  geom_errorbar(aes(x = id,
+                    ymax = as.Date("2000-01-01"),
+                    ymin = as.Date('2010-01-01'),
+                    color = status),
+                size = 1,
+                filter(ms, status != 'accepted'),
+                show.legend = T) + ## trick for making horizontal legend bars
   geom_text(aes(label = place, y = start_date, x = id),
            filter(ms, action == 'initial submission'),
            hjust = 1.8, vjust = 0.4,
@@ -51,17 +62,17 @@ fig_ms <- ggplot(ms) +
              shape = 1,
              show.legend = F) +
   geom_hline(yintercept = as.Date('2016-08-05'), linetype = 'dashed') +
-  geom_text(aes(label = '<<< PhD conferred', y = as.Date('2017-06-05'), x = 'ms02'),
+  geom_text(aes(label = '<<< PhD conferred', y = as.Date('2017-06-05'), x = 'ms03'),
             size = 3) +
-  labs(x = "Manuscript (#)") +
-  scale_y_date(name = "Year", date_breaks = "1 year", date_labels = "%Y") + 
-  scale_color_manual(name = "Status",
-                     labels = c("MS in revision", "MS under review"),
+  labs(x = "Manuscript ID") +
+  scale_y_date(name = "Year", date_breaks = "1 year", date_labels = "%Y",
+               limits = c(first(df$date), Sys.Date())) +  
+  scale_color_manual(name = "MS Status",
+                     labels = c("In revision", "Under review"),
                      values = c('black', 'grey55')) + 
-  ggtitle("Manuscript Timelines") + 
   theme(legend.background = element_rect(color = "black"),
         legend.key = element_rect(fill = 'white'),
-        legend.position = c(0.18, 0.8),
+        legend.position = c(0.1, 0.8),
         panel.background = element_rect('white'),
         panel.border = element_rect(colour = 'black', fill = "transparent"),
         panel.grid.major.x = element_line('grey', size = 0.5, linetype = "dotted"),
@@ -71,27 +82,19 @@ fig_ms <- ggplot(ms) +
   coord_flip()
 fig_ms
 
-tiff(file.path(datadir, "fig-ms.tiff"), width = 5.5, height = 5, units = "in", 
-     res = 300, compression = "lzw")
-fig_ms
-dev.off()
-
-
-##############################
-## all combined
-##############################
+## peer reviews
 rv <- df %>% filter(type == 'rv')
 
 fig_rv <- ggplot(rv) +
   geom_linerange(aes(x = id,
                      ymax = end_date,
-                     ymin = start_date,
-                     color = action),
+                     ymin = start_date),
                  size = 1,
                  show.legend = F) +
   geom_hline(yintercept = as.Date('2016-08-05'), linetype = 'dashed') +
-  labs(x = "Review ID") +
-  scale_y_date(name = "Year", date_breaks = "1 year", date_labels = "%Y") + 
+  labs(x = "Peer Review ID") +
+  scale_y_date(name = "", date_breaks = "1 year", date_labels = "%Y",
+               limits = c(first(df$date), Sys.Date())) + 
   theme(legend.background = element_rect(color = "black"),
         legend.key = element_rect(fill = 'white'),
         legend.position = c(0.18, 0.8),
@@ -99,63 +102,34 @@ fig_rv <- ggplot(rv) +
         panel.border = element_rect(colour = 'black', fill = "transparent"),
         panel.grid.major.x = element_line('grey', size = 0.5, linetype = "dotted"),
         axis.text = element_text(color = 'black'),
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
         plot.margin = margin(0.5,0.5,0.5,0.5, 'cm')) +
   coord_flip()
 fig_rv
 
+## grant proposals
 gr <- filter(df, type == 'gr')
 
 fig_gr <- ggplot(gr) +
   geom_linerange(aes(x = id,
                      ymax = end_date,
-                     ymin = start_date,
-                     color = action),
+                     ymin = start_date),
                  size = 1,
                  show.legend = F) +
   geom_text(aes(label = place, y = start_date, x = id),
             filter(gr, action %in% c('proposal submitted')),
             hjust = 1.8, vjust = 0.4,
             size = 4) +
-  geom_hline(yintercept = as.Date('2016-08-05'), linetype = 'dashed') +
-  labs(x = "Grant Proposal ID") +
-  scale_y_date(name = "Year", date_breaks = "1 year", date_labels = "%Y") + 
-  theme(legend.background = element_rect(color = "black"),
-        legend.key = element_rect(fill = 'white'),
-        legend.position = c(0.18, 0.8),
-        panel.background = element_rect('white'),
-        panel.border = element_rect(colour = 'black', fill = "transparent"),
-        panel.grid.major.x = element_line('grey', size = 0.5, linetype = "dotted"),
-        axis.text = element_text(color = 'black'),
-        plot.margin = margin(0.5,0.5,0.5,0.5, 'cm')) +
-  coord_flip()
-fig_gr
-
-fig_all <- ggplot(df2) +
-  geom_linerange(aes(x = id,
-                     ymax = end_date,
-                     ymin = start_date,
-                     color = status),
-                 size = 1,
-                 filter(df2, !status %in% c('accepted', 'published')),
-                 show.legend = F) +
-  geom_text(aes(label = place, y = start_date, x = id),
-            filter(df2, action %in% c('initial submission', 'proposal submitted')),
-            hjust = 1.8, vjust = 0.4,
-             size = 4) +
-  geom_point(aes(y = start_date, x = id),
-             filter(ms, status == 'accepted'),
+  geom_point(aes(y = end_date, x = id),
+             filter(gr, status == 'awarded'),
              size = 2,
              shape = 1,
              show.legend = F) +
   geom_hline(yintercept = as.Date('2016-08-05'), linetype = 'dashed') +
-  geom_text(aes(label = '<<< PhD conferred', y = as.Date('2017-06-05'), x = 'ms05'),
-            size = 3) +
-  labs(x = "Productivity ID") +
-  scale_y_date(name = "Year", date_breaks = "1 year", date_labels = "%Y") + 
-  # scale_color_manual(name = "Status",
-  #                    labels = c("In revision", "Under review", "Peer Review"),
-  #                    values = c('black', 'grey55', 'grey85')) + 
-  # ggtitle("Productivity") + 
+  labs(x = "Grant Proposal ID") +
+  scale_y_date(name = "", date_breaks = "1 year", date_labels = "%Y",
+               limits = c(first(df$date), Sys.Date())) + 
   theme(legend.background = element_rect(color = "black"),
         legend.key = element_rect(fill = 'white'),
         legend.position = c(0.18, 0.8),
@@ -163,18 +137,17 @@ fig_all <- ggplot(df2) +
         panel.border = element_rect(colour = 'black', fill = "transparent"),
         panel.grid.major.x = element_line('grey', size = 0.5, linetype = "dotted"),
         axis.text = element_text(color = 'black'),
-        plot.margin = margin(0.5,0.5,0.5,0.5, 'cm')) +
-  labs(caption = "*Leading number indicates author/co-PI position.") + 
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
+        plot.margin = margin(0.5,1.5,0.5,0.5, 'cm')) +
   coord_flip()
-fig_all
+fig_gr
 
-tiff(file.path(datadir, "fig-all.tiff"), width = 6, height = 6, units = "in", 
-     res = 300, compression = "lzw")
-fig_all
-dev.off()
+# legd <- legendGrob(c('Published/Awarded', 1, 5, pch = 2))
 
-facet_grid()
-tiff(file.path(datadir, "fig-all-faceted.tiff"), width = 6, height = 6, units = "in", 
+## combining all
+tiff(file.path(datadir, "fig-faceted.tiff"), width = 6.5, height = 9, units = "in", 
      res = 300, compression = "lzw")
-fig_all + facet_grid(rows = vars(type))
+# grid.arrange(fig_rv, fig_gr, fig_ms, ncol = 1, nrow = 3)
+plot_grid(fig_rv, fig_gr, fig_ms, align = 'v', nrow = 3, rel_heights = c(1/3, 1/4, 1/2))
 dev.off()
